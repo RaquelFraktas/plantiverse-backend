46348eff718b0165fb471010c57b98ab8b7a68d2
github username <github email address>
2021-11-09 20:17:04 -0500

    add origin info to scraper. add create method in plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,9 @@
 class Plant < ApplicationRecord
+
+  def self.create_from_collection(plants)
+    plants.each do |plant_hash|
+      Plant.create(plant_hash)
+    end
+  end
+
 end
--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -20,22 +20,42 @@ class Scraper
   end
 
   def scrape_plant_pages(plants_urls)
-    plants_list = []
+    plant_names = []
+    plant_details = []
 
     plants_urls.each do |plant_url|
       url = "http://www.tropicopia.com/house-plant#{plant_url.gsub("..", "")}"
       doc = Nokogiri::HTML(URI.open(url))
 
-      plants_list << doc.css('.title16')
+      plant_names << doc.css('.title16')
+      plant_details << doc.css("table[id~='Field list']")
     #   .match(/.*?\A[^-]*\b/) this grabs the latin name of the plant
-
     end
-    
-   byebug
+    create_plants(plant_names, plant_details)
+  end
+
+
+  def create_plants(plant_names, plant_details)
+   
+    plant_names.each do |plant|
+      plant_array = plant.text.split(" ")
+      latin_name = plant_array.slice(1,2).join(" ")
+      common_names= plant_array.slice(4...).join(" ")
+    end
+
+    plant_details.each do |plant|
+
+      plant_info_array= plant.css(".ar12D").text.split(" ")
+      origin = [plant_info_array[plant_info_array.find_index("Origin") + 2]] 
+          
+      if plant_info_array[plant_info_array.find_index("Origin") + 3] != "Climat"
+        origin << plant_info_array.slice(plant_info_array.find_index("Origin") + 3, 5).join(" ").match(/.+?(?=Climat)/)
+      end 
+      origin.join(" ")
+    end
 
   end
 
-  
 
 end
 
