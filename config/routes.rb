cafdbecf3563bce9318c57a5ede98f1669ab671a
github username <github email address>
2021-11-13 19:46:52 -0500

    add JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :plants
+  resources :users, only [:create]
+  resources :plants, only [:index, :show]
+
+  post "/login", to "sessions#create"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
