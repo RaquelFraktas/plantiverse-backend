3992d6b63bf49ff60f050dfeb531f0ab4a4dedda
github username <github email address>
2021-11-13 22:04:00 -0500

    add auto login JWT functions.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :plants, only: [:index, :show]
 
   post "/login", to: "sessions#create"
+  get "/me", to: "sessions#autologin"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
