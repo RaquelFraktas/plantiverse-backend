d2068ceced995cc13c36bb7b8534ff09d9607d27
github username <github email address>
2021-11-17 18:31:47 -0500

    create route for sending forum topic

--- app/controllers/forum_topics_controller.rb
+++ app/controllers/forum_topics_controller.rb
@@ -5,4 +5,23 @@ class ForumTopicsController < ApplicationController
     render json: forum_topics
   end
 
+  def show
+    # forum_topic = ForumTopic.all
+    # render json: forum_topic
+  end
+
+  def create
+    forum_topic = ForumTopic.new(forum_params)
+    if forum_topic.save
+      render json: {user: ForumTopicSerializer.new(forum_topic)} 
+    else
+      render json:{errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def forum_params
+    params.permit(:title, :content, :img_url)
+  end
+
 end
