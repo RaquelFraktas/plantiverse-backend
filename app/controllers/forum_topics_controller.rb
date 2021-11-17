f908924d2c1b1398f44e4e34df641754ff41b0be
github username <github email address>
2021-11-17 16:20:38 -0500

    change typo on forum topics

--- app/controllers/forum_topics_controller.rb
+++ app/controllers/forum_topics_controller.rb
@@ -1,7 +1,7 @@
 class ForumTopicsController < ApplicationController
 
   def index
-    forum_topics = ForumTopics.all
+    forum_topics = ForumTopic.all
     render json: forum_topics
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
   get "/me", to: "sessions#autologin"
-  get "/message_boards", to: "forumtopics#index"
+  get "/message_boards", to: "forum_topics#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
