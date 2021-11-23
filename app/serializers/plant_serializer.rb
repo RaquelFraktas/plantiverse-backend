c320b344fa66b870e748cc7caeb70e9a14af7b63
github username <github email address>
2021-11-23 12:20:55 -0500

    change display data in json

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,5 +1,10 @@
 class PlantSerializer < ActiveModel::Serializer
   attributes :id, :name, :alt_name, :img_url, :description, :origin
-  has_many :user_plants
-  has_many :users, :through => :user_plants
+  # has_many :user_plants
+  # has_many :users, :through => :user_plants
+
+  # def img_url
+  #   keys.camelize(:lower) 
+  # end
+
 end
