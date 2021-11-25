b98d596f7707cfedc164a0cef6987a6ce6a4572a
github username <github email address>
2021-11-24 19:34:35 -0500

    change instance variables to local variables

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,15 +2,15 @@ class PlantsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, plants = pagy(Plant.all)
+    pagy, plants = pagy(Plant.all)
 
     render json: { plant_records: ActiveModel::Serializer::CollectionSerializer.new(
       plants, serializer: PlantSerializer),
       info: {
-        page:  @pagy.page,
-        next:  @pagy.next,
-        items: @pagy.items,
-        pagy: pagy_metadata(@pagy)
+        page:  pagy.page,
+        next:  pagy.next,
+        items: pagy.items,
+        pagy: pagy_metadata(pagy)
       }
     }
   end
