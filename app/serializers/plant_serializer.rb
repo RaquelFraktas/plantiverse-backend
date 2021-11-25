da8d52f7850efb5c11b407b743652cd52748cc83
github username <github email address>
2021-11-24 19:32:55 -0500

    hard code camel case after pagy serialization

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,10 +1,19 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :alt_name, :img_url, :description, :origin
+  attributes :id, :name, :altName, :imgUrl, :description, :origin
   # has_many :user_plants
   # has_many :users, :through => :user_plants
 
-  # def img_url
-  #   keys.camelize(:lower) 
-  # end
+  def imgUrl
+    self.object.img_url
+  end
+
+  def altName
+    self.object.alt_name
+  end
+#since the Pagy overrides the key transforms, I had to manually 
+#redefine my JSON key names.
+#from the developer - your key transforms are probably applied before you add the pagy_metadata or just skipping it. 
+# You should apply the transforms on the result of pagy_metadata before adding it to your response.
+
 
 end
