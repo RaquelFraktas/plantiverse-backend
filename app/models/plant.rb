27138498d60f7868a0d07f953ec373af1516ac96
github username <github email address>
2021-11-29 19:54:02 -0500

    add lorem ipsum to description

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,6 @@
 class Plant < ApplicationRecord
   has_many :user_plants
-  has_many :users, :through => :user_plants
+  has_many :users, through: :user_plants
 
   def self.create_from_collection(plants)
     plants.each do |plant_hash|
@@ -8,6 +8,7 @@ class Plant < ApplicationRecord
         plant.alt_name = plant_hash[:alt_name]
         plant.origin = plant_hash[:origin]
         plant.img_url = plant_hash[:img_url]
+        plant.description = BetterLorem.p(2, true, true ) 
         # why do the ID numbers in the JSON page skip around?
       end
     end
