6ee9da3e029026d6d13e5da7be7bb9779444a9d4
github username <github email address>
2021-11-29 19:54:30 -0500

    gsub out invalid data

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -8,7 +8,6 @@ class Scraper
       new_index= url_index +=1
       plant_index_url= "http://www.tropicopia.com/house-plant/list/gallery-0#{new_index.to_s}.html"
      
-    # plant_index_url= "http://www.tropicopia.com/house-plant/list/gallery-01.html"
       doc= Nokogiri::HTML(URI.open(plant_index_url))   
       plants_container = doc.css("table[id~='Results 2']")
       plant_records = plants_container.css('#record')
@@ -22,6 +21,7 @@ class Scraper
     scrape_plant_pages(plants_urls)
   end
 
+  
   def scrape_plant_pages(plants_urls)
     plant_content = []
     # plant_details = []
@@ -39,7 +39,7 @@ class Scraper
 
   def create_plants(plant_content)
     plants = []
-   
+  
     plant_content.each do |plant|
       plant_names= plant.css('.title16')
       plant_array = plant_names.inner_html.split(" ")
@@ -58,16 +58,14 @@ class Scraper
 
       find_origin = plant.css('.ar12D')[14].inner_text.split(" ").join(" ")
       find_origin== "Origin :" ? origin= plant.css('.ar12D')[15].inner_text.split(" ").join(" ") : origin= find_origin
-    
-      byebug 
       
       img_src= plant.css('.ar12D').css('img').attr('src').value
       img_url= "http://www.tropicopia.com/house-plant" +img_src.gsub("..", "")
 
         plant_info ={
-          name: latin_name,
+          name: latin_name.gsub(/[^0-9a-zA-Z, '']+/, ""),
           alt_name: common_names,
-          origin: origin,
+          origin: origin.gsub("Climat :", ""),
           img_url: img_url
         }
 
@@ -76,9 +74,20 @@ class Scraper
     plants
   end
 
-  def description_scraper(plant_name)
-    url = "https://www.gardenia.net/plant"
-  end
+  # def replace_invalid_symbols(unfiltered_plants)
+  #   plants = unfiltered_plants.each do|plant|
+  #     # byebug
+  #     if plant[:name].match(/^[^0-9a-zA-Z]+/)
+  #       plant[:name]
+  #   end
+  # end
+  #   byebug
+  #   plants
+  # end
+
+  # def description_scraper(plant_name)
+  #   url = "https://www.gardenia.net/plant"
+  # end
 
 
 end
