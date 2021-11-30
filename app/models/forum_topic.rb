5b0e35db3f9566a753efa132293dbebbf6d658fe
github username <github email address>
2021-11-29 23:27:58 -0500

    add validation for pic urls in forum topic

--- app/models/forum_topic.rb
+++ app/models/forum_topic.rb
@@ -1,5 +1,11 @@
 class ForumTopic < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many :comments
   has_many :users, through: :comments
+  
+  validates_format_of :img_url,
+    :with    => %r{\.(gif|jpe?g|png)$}i,
+    :message => "Must have an image extension",
+    :multiline => true
+
 end
