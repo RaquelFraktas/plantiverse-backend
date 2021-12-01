b5a8a5d5bef79eed4b59c8ebb6f8f7adfddd0772
github username <github email address>
2021-12-01 00:03:31 -0500

    remove dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: true
-  has_many :user_plants, dependent: :destroy
+  has_many :user_plants
   has_many :plants, through: :user_plants
   has_many :comments, dependent: :destroy
   has_many :forum_topics, dependent: :destroy
