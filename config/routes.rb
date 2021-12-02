97461f7e13c37905d2ffe01e438a60429c611955
github username <github email address>
2021-12-02 13:28:10 -0500

    tbd routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :comments, only: [:create, :index]
   resources :users, only: [:create, :update, :show, :index]
   resources :plants, only: [:index, :show]
+  # resources :user_plants, only: [:delete]
 
   post "/login", to: "sessions#create"
   get "/me", to: "sessions#autologin"
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
   post "/message_boards", to: "forum_topics#create"
   get "/message_boards/:id/comments", to: "comments#index"
   post "/message_boards/:id/comments", to: "comments#create"
+  
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
